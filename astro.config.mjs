2fec50ca07bc23ba8dec161d0067b1b45e14682c
Camilo Guzmán <89235206+axensz@users.noreply.github.com>
2025-07-17 23:57:04 -0500

    feat: Mejoras en el diseño y agregado manual tecnico

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,10 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
+const isProd = process.env.NODE_ENV === 'production';
 export default defineConfig({
   site: 'https://axensz.github.io',
-  base: '/autocoop-website',
+  base: isProd ? '/autocoop-website' : '/',
   vite: {
     plugins: [tailwindcss()]
   }
